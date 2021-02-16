ec6f7f79cf76ec69118f47237e831dd9e932c775
DESKTOP-U7S2C0K\slaer <ratovo.kvn@gmail.com>
2021-02-16 03:11:00 +0100

    Special ability (heal yourself) + solid selecting

--- Assets/RaycastController.cs
+++ Assets/RaycastController.cs
@@ -7,6 +7,8 @@ using UnityEngine.Events;
 public class RaycastController : MonoBehaviour
 {
     public bool playerTurn;
+    private bool targetMode;
+    private int counter;
     private GameObject selectedTank = null;
     private GameObject targetedTank = null;
     private int uiLayer = 1 << 5;
@@ -16,6 +18,7 @@ public class RaycastController : MonoBehaviour
     [SerializeField] private Material tankTextureSelected;
     [SerializeField] private Camera arCam;
     [SerializeField] private GameObject effectUI;
+    [SerializeField] private GameObject targetUI;
     private bool dragMotion;
     public static Action<GameObject,GameObject> onTankDrag;
 
@@ -24,18 +27,20 @@ public class RaycastController : MonoBehaviour
     {
         Pass.switchTurn += setPlayerTurn;
         playerTurn = true;
+        targetMode = false;
         dragMotion = false;
         effectUI.SetActive(false);
+        targetUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         RaycastSelect();
         if (selectedTank != null) 
         {
             effectUI.GetComponent<RectTransform>().position =
-                new Vector3(0, -100, 0) + arCam.WorldToScreenPoint(selectedTank.GetComponent<Transform>().position);
+                arCam.WorldToScreenPoint(selectedTank.GetComponent<Transform>().position);
             effectUI.SetActive(selectedTank.GetComponent<Renderer>().isVisible);
         }
     }
@@ -46,61 +51,74 @@ public class RaycastController : MonoBehaviour
         {
             Ray ray;
             RaycastHit hit = new RaycastHit();
-            if (Input.touches[0].phase == TouchPhase.Began)
-            {
-                Debug.Log("Touch");
-                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (!targetMode) {
+                if (Input.touches[0].phase == TouchPhase.Began)
+                {
+                    counter = 0;
+                    Debug.Log("Touch");
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, tankLayerA))
-                {
-                    selectedTank?.GetComponent<Tank>().ToggleSelect();
-                    hit.transform.gameObject.GetComponent<Tank>().ToggleSelect();
-                    selectedTank = hit.transform.gameObject;
-                    dragMotion = true;
-                    effectUI.SetActive(true);
-                }
-                else
-                {
-                    Debug.Log("Enter else");
-                    if(!Physics.Raycast(ray, out hit, Mathf.Infinity, uiLayer))
+                    if (Physics.Raycast(ray, out hit, Mathf.Infinity, tankLayerA))
                     {
-                        Debug.Log("Not clicking UI");
+                        selectedTank?.GetComponent<Tank>().Unselect();
+                        hit.transform.gameObject.GetComponent<Tank>().Select();
+                        selectedTank = hit.transform.gameObject;
+                        counter++;
+                        dragMotion = true;
+                    }
+
+                }
+
+                if (Input.touches[0].phase == TouchPhase.Moved)
+                {
+                    if (dragMotion)
+                    {
+                        Ray ray2 = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                        RaycastHit hit2;
+
+                        if (Physics.Raycast(ray2, out hit2, Mathf.Infinity, tankLayerA)
+                            && !GameObject.ReferenceEquals(selectedTank, hit2.transform.gameObject))
+                        {
+                            targetedTank = hit2.transform.gameObject;
+                            targetedTank.GetComponent<Tank>().Target();
+                        }
+                        else
+                        {
+                            targetedTank?.GetComponent<Tank>().Untarget();
+                            targetedTank = null;
+                        }
+                    }
+                }
+
+                if (Input.touches[0].phase == TouchPhase.Ended)
+                {
+                    if (dragMotion && targetedTank != null)
+                    {
+                        onTankDrag.Invoke(selectedTank, targetedTank);
+                    }
+                    if (counter == 0 && targetMode != true)
+                    {
                         effectUI.SetActive(false);
-                        selectedTank?.GetComponent<Tank>().ToggleSelect();
+                        selectedTank?.GetComponent<Tank>().Unselect();
                         selectedTank = null;
                     }
+                    
+                    dragMotion = false;
                 }
-                
-            }
-            
-            if(Input.touches[0].phase == TouchPhase.Moved)
+            }else//Target stuff
             {
-                if (dragMotion)
+                if (Input.touches[0].phase == TouchPhase.Began)
                 {
-                    Ray ray2 = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                    RaycastHit hit2;
+                    Debug.Log("Touch");
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 
-                    if (Physics.Raycast(ray2, out hit2, Mathf.Infinity, tankLayerA)
-                        && !GameObject.ReferenceEquals(selectedTank,hit2.transform.gameObject))
-                    {
-                        targetedTank = hit2.transform.gameObject;
-                        targetedTank.GetComponent<Tank>().Target();
-                    }
-                    else
-                    {
-                        targetedTank?.GetComponent<Tank>().Untarget();
-                        targetedTank = null;
+                    if (Physics.Raycast(ray, out hit, Mathf.Infinity, tankLayerA))
+                    {//selected sometimes empty
+                        selectedTank.GetComponent<Tank>().Effect(hit.transform.GetComponent<Tank>());
+                        targetMode = false;
+                        targetUI.SetActive(false);
                     }
                 }
-            }
-
-            if(Input.touches[0].phase == TouchPhase.Ended)
-            {
-                if (dragMotion && targetedTank != null)
-                {
-                    onTankDrag.Invoke(selectedTank, targetedTank);
-                }
-                dragMotion = false;
             }
         }
     }
@@ -113,5 +131,15 @@ public class RaycastController : MonoBehaviour
     public void setPlayerTurn(bool b)
     {
         playerTurn = b;
+    }
+
+    public void setTargetMode(bool b)
+    {
+        targetMode = b;
+    }
+
+    public void AntiDeselect()
+    {
+        counter++;
     }
 }
