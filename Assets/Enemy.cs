44e1b30bdcab69cf58f117869301ef9c9959010a
Bananamura <nathan.schoech@gmail.com>
2021-02-16 03:53:44 +0100

    last vuforia database version

---
+++ Assets/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ennemyTank;
+    private int hp = 100;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDestroyed())
+        {
+            //faire ce que l'on veut quand la game est finie
+            Destroy();
+        }
+    }
+
+    public bool IsDestroyed()
+    {
+        if (hp <= 0)
+        {
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    public int GetHealth()
+    {
+        return this.hp;
+    }
+
+    public void takeDamage(int damage)
+    {
+        this.hp = this.hp - damage;
+    }
+
+    public void Destroy()
+    {
+        Object.Destroy(ennemyTank);
+    }
+
+}
